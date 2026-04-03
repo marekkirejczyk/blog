1e23c75207ff4fe6c289d72c913b718b16e38faa
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2026-04-03 10:54:43 +0200

    Fix deployment

--- deploy/deploy-comments.sh
+++ deploy/deploy-comments.sh
@@ -6,4 +6,4 @@ SSH="ssh -i ~/.ssh/id_ed25519 -o IdentitiesOnly=yes"
 RSYNC="rsync -avz --delete --exclude='data/' --exclude='.env' --exclude='node_modules/'"
 
 $RSYNC -e "$SSH" comments/ "$SERVER:/var/www/comments/"
-$SSH "$SERVER" "cd /var/www/comments && npm ci --omit=dev && sudo systemctl restart comments"
+$SSH "$SERVER" "cd /var/www/comments && npm install --omit=dev && sudo systemctl restart comments"
